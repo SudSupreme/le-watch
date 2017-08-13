100f2b399cd25de925ed022c9dc39e0bb7ab7bfc
Phil <phil@suds.io>
2017-08-13 05:31:17 -0400

    new base view files added

---
+++ src/renderer/components/ItemList.vue
@@ -0,0 +1,21 @@
+<template>
+  
+</template>
+<script>
+//imports
+import Vue from 'vue'
+import Vuex from 'vuex'
+import BootstrapVue from 'bootstrap-vue'
+
+
+export default {
+  name: 'ItemList'
+}
+
+//styles to load
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+</script>
+<style lang="sass">
+
+</style>
---
+++ src/renderer/components/ItemSpec.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+<script>
+  //imports
+  import Vue from 'vue';
+  import Vuex from 'vuex';
+  import BootstrapVue from 'bootstrap-vue';
+  //styles to load
+  import 'bootstrap/dist/css/bootstrap.css';
+  import 'bootstrap-vue/dist/bootstrap-vue.css';
+  export default {
+    name: 'ItemSpec'
+  }
+</script>
+
+<style lang="sass">
+
+</style>
